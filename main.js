b3c8e9385d5462a7e009bcfd40062d2d6b507903
Kali Prasad Panda <kalip.panda@nettantra.net>
2023-04-28 08:34:18 +0530

    resolved iframe maps

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const show_div = document.getElementById("show-div");
 const mapDiv = document.getElementById("map");
 const weatherDataElement = document.getElementById("weather-div");
 const geo = document.getElementById("geo");
+
 // let map;
 
 // map view api
@@ -24,8 +25,8 @@ btn.addEventListener("click", function () {
                 //    initMap(position.coords.latitude, position.coords.longitude)
                 let lat = position.coords.latitude;
                 let long = position.coords.longitude;
-                const api = "a3db74da61fed992fdd97c3c39c48eb8";
-                const geomapapi = "AIzaSyCIJroks17eUnE_CZ4oSVadGlH7Vtl9bFU";
+                const api = 'a3db74da61fed992fdd97c3c39c48eb8';
+                const geomapapi = 'AIzaSyCIJroks17eUnE_CZ4oSVadGlH7Vtl9bFU';
                 
                 btn.style.display="none";
                 show_div.style.display="block";
@@ -35,8 +36,10 @@ btn.addEventListener("click", function () {
                 geo.innerHTML = `<span>Lat:\u00A0${lat}</span>
                 <span>Long:\u00A0${long}</span>`;
 
-                mapDiv.innerHTML=`<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3020.416949233449!2d-${long}!3d${lat}!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY!5e0!3m2!1sen!2sus!4v1618592610451!5m2!1sen!2sus" width="100%" height="250" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
-                `;
+                // mapDiv.innerHTML=`<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3020.416949233449!2d-${long}!3d${lat}!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY!5e0!3m2!1sen!2sus!4v1618592610451!5m2!1sen!2sus" width="100%" height="250" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                // `;
+
+                mapDiv.innerHTML=`<iframe src="https://www.google.com/maps?q=${lat},${long}&output=embed" width="100%" height="250" style="border:0;" allowfullscreen="" loading="lazy"></iframe>`;
 
 
                 fetch(url)
@@ -59,7 +62,3 @@ btn.addEventListener("click", function () {
         console.log(error)
     }
 );
-
-//  console.log(abc);
-
-// })
